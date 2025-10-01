d95d546911ac1576795d33cbc0c82840f83bc79b
Benino Dorsey <benino.dorsey1@marist.edu>
2025-10-01 12:02:05 -0400

    Finished Lab 4

--- app/src/main/java/com/codepath/campgrounds/Campground.kt
+++ app/src/main/java/com/codepath/campgrounds/Campground.kt
@@ -4,16 +4,37 @@ import android.support.annotation.Keep
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// TODO: Create a data class for the Data Response
+// COMPLETED: Create a data class for the Data Response
+@Keep
+@Serializable
+data class CampgroundResponse(
+    @SerialName("data")
+    val data: List<Campground>?
+)
 
-
-// TODO: Implement remaining keys of the Campground data class
+// COMPLETED: Implement remaining keys of the Campground data class
 @Keep
 @Serializable
 data class Campground(
     @SerialName("name")
     val name: String?,
+    @SerialName("description")
+    val description: String?,
+    @SerialName("latLong")
+    val latLong: String?,
+    @SerialName("images")
+    val images: List<CampgroundImage>?
+) : java.io.Serializable {
+    val imageUrl: String
+        get() = images?.firstOrNull { !it.url.isNullOrEmpty() }?.url ?: ""
+}
+
+
+
+// COMPLETED: Create a data class for the Image Response
+@Keep
+@Serializable
+data class CampgroundImage(
+    @SerialName("url") val url: String?,
+    @SerialName("title") val title: String?
 ) : java.io.Serializable
-
-
-// TODO: Create a data class for the Image Response
